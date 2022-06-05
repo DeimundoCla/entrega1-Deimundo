1cbf39fe1bf3622239a6e9557a7547b8a280ffda
Deimundo <deimundocla@gmail.com>
2022-06-05 01:08:15 -0300

    updaterama

--- swift_web/Monedas/models.py
+++ swift_web/Monedas/models.py
@@ -5,21 +5,21 @@ from datetime import date
 
 
 class Euro(models.Model):
-    nombre = "Euro"
+    nombre = models.CharField(max_length=50, default="Euro")
     simbolo = "€"
     cotizacion = models.FloatField()
     fecha = models.DateField(default=date.today)
     variacion = models.DecimalField(max_digits=3, decimal_places=0)
 
 class Dolar(models.Model):
-    nombre = "Dólar Oficial"
+    nombre = models.CharField(max_length=50, default="Dolar")
     simbolo = "U$S"
     cotizacion = models.FloatField()
     fecha = models.DateField(default=date.today)
     variacion = models.DecimalField(max_digits=3, decimal_places=0)
 
 class Dolar_blue(models.Model):
-    nombre = "Dólar Blue"
+    nombre = models.CharField(max_length=50, default="Dolar Blue")
     simbolo = "USD"
     cotizacion = models.FloatField()
     fecha = models.DateField(default=date.today)
@@ -27,7 +27,7 @@ class Dolar_blue(models.Model):
 
 
 class Reais(models.Model):
-    nombre = "Real"
+    nombre = models.CharField(max_length=50, default="Real")
     simbolo = "R$"
     cotizacion = models.FloatField()
     fecha = models.DateField(default=date.today)
--- swift_web/Monedas/views.py
+++ swift_web/Monedas/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.template import loader
-from .models import Dolar, Dolar_blue, Euro, Reais
+from sympy import Q
+from . import models
+from .forms import Usd_Form, Usd_blue_Form, Euro_Form, Real_Form, busqueda_Form
 import datetime
 
 # Create your views here.
@@ -16,13 +19,13 @@ def conversor(request):
     documento = template.render()
     return HttpResponse(documento)
 
-def contacto(request):
-    template = loader.get_template("contacto.html")
+def cotizaciones(request):
+    template = loader.get_template("cotizaciones.html")
     documento = template.render()
     return HttpResponse(documento)
 
-def cotizaciones(request):
-    template = loader.get_template("cotizaciones.html")
+def contacto(request):
+    template = loader.get_template("contacto.html")
     documento = template.render()
     return HttpResponse(documento)
 
@@ -31,33 +34,61 @@ def landing_cotizacion(request):
     documento = template.render()
     return HttpResponse(documento)
 
-def alta_usd(request):
-        if request.method == "POST":
-            usd_cotizacion = Dolar( cotizacion = request.POST["cotizacion-usd"], fecha = request.POST["fecha-cotizacion"], variacion = request.POST["variacion-cotizacion"])
-            usd_cotizacion.save()
-        return render (request, 'cotizaciones.html')
-        
+def landing_busqueda(request):
+    usd = None
+    usd_blue = None
+    euro = None
+    real = None
 
-def alta_usd_blue(request):
-    if request.method == "POST":
-        usd_blue_cotizacion = Dolar_blue( cotizacion = request.POST["cotizacion-usd-blue"], fecha = request.POST["fecha-cotizacion-blue"], variacion = request.POST["variacion-cotizacion-blue"])
-        usd_blue_cotizacion.save()
-        return render (request, 'landing_cotizacion.html')
+    if 'q' in request.GET:
+        q = request.GET['q']
+        usd = models.Dolar.objects.filter(fecha__icontains=q)
+        usd_blue = models.Dolar_blue.objects.filter(fecha__icontains=q)
+        euro = models.Euro.objects.filter(fecha__icontains=q)
+        real = models.Reais.objects.filter(fecha__icontains=q)
     else:
-        return render (request, 'cotizaciones.html')
+        q = (models.Dolar.objects.all, models.Dolar_blue.objects.all, models.Euro.objects.all, models.Reais.objects.all)
+    
+    
+    return render (request, 'landing_busqueda.html', {'usd': usd, 'usd_blue': usd_blue, 'euro': euro, 'real': real})
 
-def alta_euro(request):
+def cotizacion_usd(request):
     if request.method == "POST":
-        euro_cotizacion = Euro( cotizacion = request.POST["cotizacion-euro"], fecha = request.POST["fecha-cotizacion_euro"], variacion = request.POST["variacion-cotizacion_euro"])
-        euro_cotizacion.save()
-        return render (request, 'landing_cotizacion.html')
-    else:
-        return render (request, 'cotizaciones.html')
+        form_usd = Usd_Form(request.POST)
+        if form_usd.is_valid():
+            form_usd.save()
+            return HttpResponseRedirect("/landing_cotizacion/")
+        else:
+            return render(request, "cotizacion_usd.html",  {'form': form_usd})
+    form = Usd_Form
+    return render(request, "cotizacion_usd.html",  {'form': form})
 
-def alta_real(request):
+def cotizacion_usd_blue(request):
     if request.method == "POST":
-        real_cotizacion = Reais( cotizacion = request.POST["cotizacion-real"], fecha = request.POST["fecha-cotizacion_real"], variacion = request.POST["variacion-cotizacion_real"])
-        real_cotizacion.save()
-        return render (request, 'landing_cotizacion.html')
-    else:
-        return render (request, 'cotizaciones.html')
+        form_usd_blue = Usd_blue_Form(request.POST)
+        if form_usd_blue.is_valid():
+            form_usd_blue.save()
+            return HttpResponseRedirect("/landing_cotizacion/")
+    
+    form = Usd_blue_Form
+    return render(request, "cotizacion_usd_blue.html",  {'form': form})
+
+def cotizacion_euro(request):
+    if request.method == "POST":
+        form_euro = Euro_Form(request.POST)
+        if form_euro.is_valid():
+            form_euro.save()
+            return HttpResponseRedirect("/landing_cotizacion/")
+    
+    form = Euro_Form
+    return render(request, "cotizacion_euro.html",  {'form': form})
+
+def cotizacion_real(request):
+    if request.method == "POST":
+        form_real = Real_Form(request.POST)
+        if form_real.is_valid():
+            form_real.save()
+            return HttpResponseRedirect("/landing_cotizacion/")
+    
+    form = Real_Form
+    return render(request, "cotizacion_real.html",  {'form': form})
